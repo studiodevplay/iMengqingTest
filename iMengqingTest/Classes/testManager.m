fa10fc4a6bae0cf07bbe3bda44d3d3237b83e130
iMengqing <mengqing.gao@mintegral.com>
2023-08-02 17:05:17 +0800

    first commit

---
+++ iMengqingTest/Classes/testManager.m
@@ -0,0 +1,8 @@
+//
+//  testManager.m
+//  iMengqingTest
+//
+//  Created by iMengqing on 2023/8/2.
+//
+
+#import <Foundation/Foundation.h>
---
+++ testManager.h
@@ -0,0 +1,12 @@
+//
+//  testManager.h
+//  iMengqingTest
+//
+//  Created by iMengqing on 2023/8/2.
+//
+
+#ifndef testManager_h
+#define testManager_h
+
+
+#endif /* testManager_h */
